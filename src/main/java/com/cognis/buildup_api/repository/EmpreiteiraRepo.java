5bf45f4609affd27d766c7110f402d60a51a0e94
vitormarcao <vitormarcao100@gmail.com>
2026-03-12 22:36:03 -0300

    Feat: Criei um repositório da empreiteira

---
+++ src/main/java/com/cognis/buildup_api/repository/EmpreiteiraRepo.java
@@ -0,0 +1,9 @@
+package com.cognis.buildup_api.repository;
+
+import com.cognis.buildup_api.core.Empreiteira;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpreiteiraRepo extends JpaRepository<Empreiteira, Integer> {
+
+
+}
