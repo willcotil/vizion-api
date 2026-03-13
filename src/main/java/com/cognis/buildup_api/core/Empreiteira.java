91331c7029b4fa98332957737918bc31d0b32d29
vitormarcao <vitormarcao100@gmail.com>
2026-03-12 22:24:54 -0300

    FEAT:  entidade empreiteira

---
+++ src/main/java/com/cognis/buildup_api/core/Empreiteira.java
@@ -0,0 +1,27 @@
+package com.cognis.buildup_api.core;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Empreiteira {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 18, nullable = false)
+    private String cnpj;
+
+    @Column(length = 150, nullable = true)
+    private String nome_fantasia;
+
+    @Column(length = 150, nullable = false)
+    private String razao_social;
+
+}
