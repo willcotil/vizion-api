d95036b12c09a29904c85ba48471afe6b789fff0
abilavitoria <toriaraujo6@gmail.com>
2026-03-12 22:47:48 -0300

    feat: criei o enderecoRepository

---
+++ src/main/java/com/cognis/buildup_api/repository/EnderecoRepo.java
@@ -0,0 +1,7 @@
+package com.cognis.buildup_api.repository;
+
+import com.cognis.buildup_api.core.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepo extends JpaRepository<Endereco, Integer> {
+}
