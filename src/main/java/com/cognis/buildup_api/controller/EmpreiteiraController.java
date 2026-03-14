202f8676fe0d6ae0e9a89c030a5f4af0b670d39d
vitormarcao <vitormarcao100@gmail.com>
2026-03-14 11:03:10 -0300

    Feat: add uma Api de buscar por ID e uma de inserir dados

--- src/main/java/com/cognis/buildup_api/controller/EmpreiteiraController.java
+++ src/main/java/com/cognis/buildup_api/controller/EmpreiteiraController.java
@@ -1,12 +1,12 @@
 package com.cognis.buildup_api.controller;
 
-import com.cognis.buildup_api.core.Empreiteira;
+import com.cognis.buildup_api.core.empreiteira.Empreiteira;
 import com.cognis.buildup_api.repository.EmpreiteiraRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController("/api/empreiteira")
 public class EmpreiteiraController {
@@ -24,4 +24,16 @@ public class EmpreiteiraController {
     public void deletar(@RequestBody Empreiteira body){
         empreiteiraRepo.delete(body);
     }
+
+    @GetMapping
+    public ResponseEntity<Empreiteira> buscarPorId(@RequestBody Integer id){
+        Optional<Empreiteira> buscarPorId = empreiteiraRepo.findById(id);
+        return ResponseEntity.of(buscarPorId);
+    }
+
+    @PostMapping
+    public Empreiteira inserirDados(@RequestBody Empreiteira body){
+        return empreiteiraRepo.save(body);
+    }
+
 }
