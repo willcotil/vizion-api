f923b12256d0455d41ffc764a2aad9eddd945539
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-14 10:55:32 -0300

    feat: client controller

---
+++ src/main/java/com/cognis/buildup_api/controller/ClienteController.java
@@ -0,0 +1,25 @@
+package com.cognis.buildup_api.controller;
+
+import com.cognis.buildup_api.core.cliente.Cliente;
+import com.cognis.buildup_api.core.cliente.ClienteRequest;
+import com.cognis.buildup_api.core.cliente.ClienteResponse;
+import com.cognis.buildup_api.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/cliente")
+@RequiredArgsConstructor
+public class ClienteController {
+
+    private final ClienteService service;
+
+    @PostMapping
+    public ResponseEntity<ClienteResponse> salvar(@RequestBody ClienteRequest clienteRequest){
+        ClienteResponse response = service.salvar(clienteRequest);
+        return ResponseEntity.ok(response);
+    }
+
+}
