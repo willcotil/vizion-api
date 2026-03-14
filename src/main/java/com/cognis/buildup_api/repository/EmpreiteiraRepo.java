f0dec61a51d7b037f9565417cbbdabb307688969
vitormarcao <vitormarcao100@gmail.com>
2026-03-14 11:05:32 -0300

    Feat: mudei diretorio

--- src/main/java/com/cognis/buildup_api/repository/EmpreiteiraRepo.java
+++ src/main/java/com/cognis/buildup_api/repository/EmpreiteiraRepo.java
@@ -1,6 +1,6 @@
 package com.cognis.buildup_api.repository;
 
-import com.cognis.buildup_api.core.Empreiteira;
+import com.cognis.buildup_api.core.empreiteira.Empreiteira;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpreiteiraRepo extends JpaRepository<Empreiteira, Integer> {
