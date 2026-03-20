501e8b87b4d2357c3865515d292a6d7fd9686087
felipe-de-paula-dev <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-19 21:00:22 -0300

    refactor: mudando diretorio do arquivo usuario

---
+++ src/main/java/com/cognis/buildup_api/core/usuario/Usuario.java
@@ -0,0 +1,90 @@
+package com.cognis.buildup_api.core.usuario;
+
+import com.cognis.buildup_api.core.empreiteira.Empreiteira;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 155, nullable = false)
+    private String nome;
+
+    @CPF
+    @Column(length = 14, nullable = false)
+    private String cpf;
+
+    @Email
+    @Column(length = 150, nullable = false)
+    private String email;
+
+    @Column(length = 255, nullable = false)
+    private String senha;
+
+    @Column(length = 20, nullable = false)
+    private String status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UsuarioRole role;
+
+    @Column(length = 50)
+    private String tenant_id;
+
+    @OneToOne
+    @JoinColumn(name = "id_empreiteira")
+    private Empreiteira empreiteira;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
