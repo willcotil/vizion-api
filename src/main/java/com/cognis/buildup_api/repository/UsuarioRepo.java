88722f0790a2dd27ad4f8f2df929299de7a78b1a
felipe-de-paula-dev <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-19 21:00:42 -0300

    refactor: mudando importacao do repository

--- src/main/java/com/cognis/buildup_api/repository/UsuarioRepo.java
+++ src/main/java/com/cognis/buildup_api/repository/UsuarioRepo.java
@@ -1,6 +1,6 @@
 package com.cognis.buildup_api.repository;
 
-import com.cognis.buildup_api.core.Usuario;
+import com.cognis.buildup_api.core.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepo extends JpaRepository<Usuario, Integer> {
