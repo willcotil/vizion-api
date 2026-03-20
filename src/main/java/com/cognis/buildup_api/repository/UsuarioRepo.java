9c65ca39de742a98c508c8547fe3bb90f1fa8d2c
felipe-de-paula-dev <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-19 21:13:54 -0300

    feat: novo metodo findbyemail

--- src/main/java/com/cognis/buildup_api/repository/UsuarioRepo.java
+++ src/main/java/com/cognis/buildup_api/repository/UsuarioRepo.java
@@ -3,5 +3,8 @@ package com.cognis.buildup_api.repository;
 import com.cognis.buildup_api.core.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepo extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByEmail(String email);
 }
