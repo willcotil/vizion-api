2ae8315b200fbca1f45396215e26e9ab6bf1d3d2
abilavitoria <toriaraujo6@gmail.com>
2026-03-21 10:54:28 -0300

    feat:fiz a classe endereco

--- src/main/java/com/cognis/buildup_api/core/endereco/Endereco.java
+++ src/main/java/com/cognis/buildup_api/core/endereco/Endereco.java
@@ -1,4 +1,32 @@
 package com.cognis.buildup_api.core.endereco;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 255, nullable = false)
+    private String logradouro;
+    @Column(length = 20, nullable = false)
+    private String numero;
+    @Column(length = 100)
+    private String complemento;
+    @Column(length = 100, nullable = false)
+    private String bairro;
+    @Column(length = 100, nullable = false)
+    private String cidade;
+    @Column(length = 2, nullable = false)
+    private String estado;
+    @Column(length = 10)
+    private String cep;
 }
