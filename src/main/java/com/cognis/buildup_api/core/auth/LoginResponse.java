2e69c08ce1e2ff051f836b02399ee7c8e3a6fa3b
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-24 13:56:34 -0300

    feat: dto para auth

---
+++ src/main/java/com/cognis/buildup_api/core/auth/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.cognis.buildup_api.core.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String nome;
+    private String role;
+    private String tenantId;
+}
