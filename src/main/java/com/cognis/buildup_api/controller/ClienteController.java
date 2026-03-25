17ff33a5ca7ecee79a0ae41a9cac635ff83f9866
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-25 20:28:40 -0300

    feat: implementando herança

--- src/main/java/com/cognis/buildup_api/controller/ClienteController.java
+++ src/main/java/com/cognis/buildup_api/controller/ClienteController.java
@@ -6,22 +6,14 @@ import com.cognis.buildup_api.core.cliente.ClienteResponse;
 import com.cognis.buildup_api.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
-@RequiredArgsConstructor
-public class ClienteController {
-
-    private final ClienteService service;
-
-    @PostMapping
-    public ResponseEntity<ClienteResponse> salvar(@RequestBody ClienteRequest clienteRequest){
-        ClienteResponse response = service.salvar(clienteRequest);
-        return ResponseEntity.ok(response);
+public class ClienteController extends GenericController<ClienteRequest, ClienteResponse, Integer> {
+    public ClienteController(ClienteService service) {
+        super(service);
     }
-
 }
