e096bdb0d9817da0a68867eb3ea3f1403b70ff61
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-25 20:28:57 -0300

    feat: interceptor de excecoes

---
+++ src/main/java/com/cognis/buildup_api/infra/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.cognis.buildup_api.infra.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        if (ex.getMessage().contains("não encontrado")) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+}
