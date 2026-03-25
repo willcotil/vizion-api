c9c3e2c9c53a311d154063747f14b811c0537ec9
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-25 20:28:28 -0300

    feat: controller generico

---
+++ src/main/java/com/cognis/buildup_api/controller/GenericController.java
@@ -0,0 +1,43 @@
+package com.cognis.buildup_api.controller;
+
+import com.cognis.buildup_api.service.IGenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public abstract class  GenericController<REQ, RES, ID> {
+
+    protected final IGenericService<REQ, RES, ID> service;
+
+    protected GenericController(IGenericService<REQ, RES, ID> service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<RES> save(@RequestBody REQ request) {
+        return ResponseEntity.ok(service.salvar(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RES> update(@RequestBody REQ request, @PathVariable ID id) {
+        return ResponseEntity.ok(service.atualizar(request, id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RES>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RES> getOne(@PathVariable ID id) {
+        return ResponseEntity.ok(service.getOne(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteOne(@PathVariable ID id) {
+        service.deleteOne(id);
+        return ResponseEntity.ok("Deletado Com Sucesso");
+    }
+}
