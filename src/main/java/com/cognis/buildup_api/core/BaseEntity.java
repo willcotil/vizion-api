5905fcb2d4d978eff695fc59edd2ab1737b0984c
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-25 23:16:54 -0300

    feat: nova classe base para entidade

---
+++ src/main/java/com/cognis/buildup_api/core/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.cognis.buildup_api.core;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreationTimestamp
+    @Column(name = "data_criacao", updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+}
