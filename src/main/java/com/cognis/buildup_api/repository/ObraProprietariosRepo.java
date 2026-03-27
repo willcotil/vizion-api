d16a8fae948b54176c60e2d8c92cb6a92b331942
vitormarcao <vitormarcao100@gmail.com>
2026-03-27 11:44:56 -0300

    Feat: apenas add um "s"

--- src/main/java/com/cognis/buildup_api/repository/ObraProprietariosRepo.java
+++ src/main/java/com/cognis/buildup_api/repository/ObraProprietariosRepo.java
@@ -3,5 +3,5 @@ package com.cognis.buildup_api.repository;
 import com.cognis.buildup_api.core.obra.obra_proprietarios.ObraProprietarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ObraProprietarioRepo extends JpaRepository<ObraProprietarios, Integer> {
+public interface ObraProprietariosRepo extends JpaRepository<ObraProprietarios, Integer> {
 }
