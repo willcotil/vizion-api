c3c11151981cdc0f08017eff1444c0dd788a191b
vitormarcao <vitormarcao100@gmail.com>
2026-03-27 10:36:31 -0300

    Feat: fiz o repositorio e fiz a herança dele no jpa

--- src/main/java/com/cognis/buildup_api/repository/ObraRepo.java
+++ src/main/java/com/cognis/buildup_api/repository/ObraRepo.java
@@ -1,4 +1,7 @@
 package com.cognis.buildup_api.repository;
 
-public interface ObraRepo {
+import com.cognis.buildup_api.core.obra.Obra;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObraRepo extends JpaRepository<Obra, Integer> {
 }
