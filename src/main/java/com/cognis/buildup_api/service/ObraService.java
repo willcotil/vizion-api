5902c738b2a0e16b2c8c3895240c5af468f3b070
vitormarcao <vitormarcao100@gmail.com>
2026-03-27 11:14:25 -0300

    Feat: fiz o obra service

---
+++ src/main/java/com/cognis/buildup_api/service/ObraService.java
@@ -0,0 +1,25 @@
+package com.cognis.buildup_api.service;
+
+import com.cognis.buildup_api.core.BaseEntity;
+import com.cognis.buildup_api.core.cliente.Cliente;
+import com.cognis.buildup_api.core.cliente.ClienteResponse;
+import com.cognis.buildup_api.core.obra.Obra;
+import com.cognis.buildup_api.core.obra.ObraRequest;
+import com.cognis.buildup_api.core.obra.ObraResponse;
+import com.cognis.buildup_api.repository.ObraRepo;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ObraService extends BaseService<Obra, ObraRequest, ObraResponse, Integer> {
+    private final ObraRepo repo;
+    private final ModelMapper mapper;
+
+    @Override protected JpaRepository<Obra, Integer> getRepo() { return repo; }
+    @Override protected ModelMapper getMapper() { return mapper; }
+    @Override protected Class<Obra> getEntityClass() { return Obra.class; }
+    @Override protected Class<ObraResponse> getResponseClass() { return ObraResponse.class; }
+}
