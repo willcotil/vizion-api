0dd8d1ee797b7a03d53b613e10509717013c2252
felipe-de-paula-dev <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-26 22:23:30 -0300

    feat: request para entidades

--- src/main/java/com/cognis/buildup_api/core/usuario/UsuarioRequest.java
+++ src/main/java/com/cognis/buildup_api/core/usuario/UsuarioRequest.java
@@ -5,12 +5,8 @@ import lombok.Data;
 @Data
 
 public class UsuarioRequest {
-    private String nome;
-    private String cpf;
     private String email;
     private String senha;
     private String status;
     private UsuarioRole role;
-    private String tenant_id;
-
 }
