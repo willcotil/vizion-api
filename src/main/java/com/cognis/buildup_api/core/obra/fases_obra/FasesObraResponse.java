771c611e5bde35aa0cd1a6e127c20d6b3d9135ad
vitormarcao <vitormarcao100@gmail.com>
2026-03-27 11:06:19 -0300

    Feat: fiz request e response da entidade fases obra

---
+++ src/main/java/com/cognis/buildup_api/core/obra/fases_obra/FasesObraResponse.java
@@ -0,0 +1,22 @@
+package com.cognis.buildup_api.core.obra.fases_obra;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class FasesObraResponse {
+
+    private int id_fase;
+    private String nome_fase;
+    private String descricao;
+    private Date data_inicio_estimada;
+    private Date data_iniciada;
+    private Date data_fim_estimada;
+    private Date data_finalizada;
+    private BigDecimal valor_estimado;
+    private BigDecimal valor_pago;
+    private String status;
+    private int ordem_exibicao;
+}
