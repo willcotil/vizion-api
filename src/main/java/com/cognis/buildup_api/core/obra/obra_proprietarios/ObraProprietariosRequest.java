5723b8ebd0ff3aaefd52a4a1ddc1be8236a7f6c1
vitormarcao <vitormarcao100@gmail.com>
2026-03-27 11:14:14 -0300

    Feat: fiz request da entidade obra proprietarios

---
+++ src/main/java/com/cognis/buildup_api/core/obra/obra_proprietarios/ObraProprietariosRequest.java
@@ -0,0 +1,10 @@
+package com.cognis.buildup_api.core.obra.obra_proprietarios;
+
+import lombok.Data;
+
+@Data
+public class ObraProprietariosRequest {
+
+    private int id_obra;
+    private int id_cliente;
+}
