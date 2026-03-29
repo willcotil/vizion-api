0ed3d4bd1bd562fac633f8df3f5114a87ea99fac
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-03-28 23:34:57 -0300

    delete: excluindo arquivos para fazer alteracoes de diretorio

--- src/main/java/com/cognis/buildup_api/core/obra/fases_obra/obra_material/ObraMaterialRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cognis.buildup_api.core.obra.fases_obra.obra_material;
-
-import lombok.Data;
-
-@Data
-public class ObraMaterialRequest {
-
-    private String nome_material;
-}
--- src/main/java/com/cognis/buildup_api/core/obra/obra_proprietarios/ObraProprietariosResponse.java
+++
@@ -1,7 +0,0 @@
-package com.cognis.buildup_api.core.obra.obra_proprietarios;
-
-public class ObraProprietariosResponse {
-
-    private int id_obra;
-    private int id_cliente;
-}
