7a9f7c7550e9d3803639d8970b4be665d45080a6
felipe-de-paula-dev <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-04-11 10:41:50 -0300

    feat: ajustando mapeamento de entidade

--- src/main/java/com/cognis/vizion/api/core/obra/Obra.java
+++ src/main/java/com/cognis/vizion/api/core/obra/Obra.java
@@ -2,24 +2,23 @@ package com.cognis.vizion.api.core.obra;
 
 import com.cognis.vizion.api.core.BaseEntity;
 import com.cognis.vizion.api.core.endereco.Endereco;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import com.cognis.vizion.api.core.obra.obraFinanceiro.ObraFinanceiro;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Obra extends BaseEntity{
+public class Obra extends BaseEntity {
 
     @Column(length = 50, nullable = false)
     private String tenant_id;
-    @Column(length = 150, nullable= false)
+    @Column(length = 150, nullable = false)
     private String nome_projeto;
     @OneToOne
     @JoinColumn(name = "id_endereco")
@@ -32,4 +31,7 @@ public class Obra extends BaseEntity{
     private BigDecimal valor_total_estimado;
     @Column(length = 20, nullable = false)
     private String status;
+
+    @OneToMany(mappedBy = "obra", cascade = CascadeType.ALL)
+    private List<ObraFinanceiro> financeiro;
 }
