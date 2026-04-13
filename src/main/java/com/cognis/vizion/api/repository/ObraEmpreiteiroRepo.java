b5a5d26aa73ca68a245df4f830add3f03cd770c7
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-04-13 10:50:17 -0300

    feat: add ObraEmpreiteiroRepo

---
+++ src/main/java/com/cognis/vizion/api/repository/ObraEmpreiteiroRepo.java
@@ -0,0 +1,7 @@
+package com.cognis.vizion.api.repository;
+
+import com.cognis.vizion.api.core.obra.obraEmpreiteiro.ObraEmpreiteiro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObraEmpreiteiroRepo extends JpaRepository<ObraEmpreiteiro, Integer> {
+}
