a49823c05976ec73cc7d742e2e8939cbd52f48a9
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-04-13 10:47:51 -0300

    feat: enhance ObraFinanceiro with type and Material FK

--- src/main/java/com/cognis/vizion/api/core/obra/obraFinanceiro/ObraFinanceiro.java
+++ src/main/java/com/cognis/vizion/api/core/obra/obraFinanceiro/ObraFinanceiro.java
@@ -1,17 +1,37 @@
 package com.cognis.vizion.api.core.obra.obraFinanceiro;
 
 import com.cognis.vizion.api.core.BaseEntity;
+import com.cognis.vizion.api.core.estoque.Material;
 import com.cognis.vizion.api.core.obra.Obra;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "obra_financeiro")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class ObraFinanceiro extends BaseEntity {
+
+    @Column(length = 50, nullable = false)
+    private String tenant_id;
+
     @Column(length = 200, nullable = false)
     private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoFinanceiro tipo; 
 
     @Column(precision = 21, scale = 6, nullable = false)
     private BigDecimal valor;
@@ -23,9 +43,13 @@ public class ObraFinanceiro extends BaseEntity {
     private LocalDate dataEstimado;
 
     @Column(nullable = false)
-    private boolean pago;
+    private boolean pago = false;
 
     @ManyToOne
     @JoinColumn(name = "id_obra", nullable = false)
     private Obra obra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_material")
+    private Material material;
 }
--- src/main/java/com/cognis/vizion/api/core/obra/obraFinanceiro/dto/ObraFinanceiroResponse.java
+++ src/main/java/com/cognis/vizion/api/core/obra/obraFinanceiro/dto/ObraFinanceiroResponse.java
@@ -1,17 +1,20 @@
 package com.cognis.vizion.api.core.obra.obraFinanceiro.dto;
 
+import com.cognis.vizion.api.core.obra.obraFinanceiro.ObraFinanceiro;
 import lombok.Data;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
 
 @Data
 public class ObraFinanceiroResponse {
     private Integer id;
+    private String tenant_id;
     private String descricao;
+    private ObraFinanceiro.TipoFinanceiro tipo_financeiro;
     private BigDecimal valor;
     private LocalDate dataPagamento;
     private LocalDate dataEstimado;
     private boolean pago;
+    private Integer idObra;
+    private Integer idMaterial;
 }
