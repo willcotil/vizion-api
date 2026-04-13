33a22f15d5a4173715ccbc6f59185d36bfee15dd
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-04-13 10:50:18 -0300

    feat: add ObraEmpreiteiroService

---
+++ src/main/java/com/cognis/vizion/api/service/ObraEmpreiteiroService.java
@@ -0,0 +1,23 @@
+package com.cognis.vizion.api.service;
+
+import com.cognis.vizion.api.core.obra.obraEmpreiteiro.ObraEmpreiteiro;
+import com.cognis.vizion.api.core.obra.obraEmpreiteiro.dto.ObraEmpreiteiroRequest;
+import com.cognis.vizion.api.core.obra.obraEmpreiteiro.dto.ObraEmpreiteiroResponse;
+import com.cognis.vizion.api.repository.ObraEmpreiteiroRepo;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ObraEmpreiteiroService extends BaseService<ObraEmpreiteiro, ObraEmpreiteiroRequest, ObraEmpreiteiroResponse, Integer> {
+
+    private final ObraEmpreiteiroRepo repo;
+    private final ModelMapper mapper;
+
+    @Override protected JpaRepository<ObraEmpreiteiro, Integer> getRepo() { return repo; }
+    @Override protected ModelMapper getMapper() { return mapper; }
+    @Override protected Class<ObraEmpreiteiro> getEntityClass() { return ObraEmpreiteiro.class; }
+    @Override protected Class<ObraEmpreiteiroResponse> getResponseClass() { return ObraEmpreiteiroResponse.class; }
+}
