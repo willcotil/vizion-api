fe8a2052dfc4ac7cbfda10193663038c4f564eba
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-04-13 11:04:12 -0300

    feat: add ObraFuncionariosService

---
+++ src/main/java/com/cognis/vizion/api/service/ObraFuncionariosService.java
@@ -0,0 +1,23 @@
+package com.cognis.vizion.api.service;
+
+import com.cognis.vizion.api.core.obra.obraFuncionarios.ObraFuncionarios;
+import com.cognis.vizion.api.core.obra.obraFuncionarios.dto.ObraFuncionariosRequest;
+import com.cognis.vizion.api.core.obra.obraFuncionarios.dto.ObraFuncionariosResponse;
+import com.cognis.vizion.api.repository.ObraFuncionariosRepo;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ObraFuncionariosService extends BaseService<ObraFuncionarios, ObraFuncionariosRequest, ObraFuncionariosResponse, Integer> {
+
+    private final ObraFuncionariosRepo repo;
+    private final ModelMapper mapper;
+
+    @Override protected JpaRepository<ObraFuncionarios, Integer> getRepo() { return repo; }
+    @Override protected ModelMapper getMapper() { return mapper; }
+    @Override protected Class<ObraFuncionarios> getEntityClass() { return ObraFuncionarios.class; }
+    @Override protected Class<ObraFuncionariosResponse> getResponseClass() { return ObraFuncionariosResponse.class; }
+}
