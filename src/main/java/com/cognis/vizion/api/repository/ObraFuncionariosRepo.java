9be203900b4accbc0baca54e768ff3af04fa5c8f
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-04-13 11:04:11 -0300

    feat: add ObraFuncionariosRepo

---
+++ src/main/java/com/cognis/vizion/api/repository/ObraFuncionariosRepo.java
@@ -0,0 +1,7 @@
+package com.cognis.vizion.api.repository;
+
+import com.cognis.vizion.api.core.obra.obraFuncionarios.ObraFuncionarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObraFuncionariosRepo extends JpaRepository<ObraFuncionarios, Integer> {
+}
